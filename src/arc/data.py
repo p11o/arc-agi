85bfc011bcca4264f47bfeee5dd41d6d5c77ca85
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-09-23 20:01:52 -0400

    load all data

--- src/arc/data.py
+++ src/arc/data.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def read(path: str):
@@ -7,3 +8,27 @@ def read(path: str):
     
 challenges = read('/data/arc-agi_training_challenges.json')
 solutions = read('/data/arc-agi_training_solutions.json')
+
+def to_df(challenges, solutions):
+    """
+    id: {group} + {group}.train|test.idx
+    group_id: {group}
+    type: test|train
+    input: {group} | (.train[], .test[]) | .input
+    output: {group} | (.train[].output) 
+    """
+    df_array = []
+    for group_id, group in challenges.items():
+        data = [*group["train"], *[{**test, "type": "test", "output": solutions[group_id][i]} for i, test in enumerate(group["test"])]]
+        for i, datum in enumerate(data):
+            _input, _output = datum["input"], datum["output"]
+
+            try:
+                df_array.append([f"{group_id}{i}", group_id, datum.get("type", "train"), pd.array(_input), pd.array(_output)])
+            except Exception as e:
+                print("input", _input)
+                print("output", _output)
+                raise e
+    return pd.DataFrame(df_array, columns=["id", "group_id", "type", "input", "output"])
+
+
